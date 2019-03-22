74f3fcc4fda56967237472da26d5f42926b1b20c
JpRod <jprodriguez60@gmail.com>
2019-03-22 15:28:52 -0400

    Se quito librerias sin usar y se cambio la anotacion requestmapping por postmapping

--- src/main/java/com/ucbcba/joel/ucbcorreccionformato/FormalAspects/BasicFormatController.java
+++ src/main/java/com/ucbcba/joel/ucbcorreccionformato/FormalAspects/BasicFormatController.java
@@ -1,7 +1,6 @@
 package com.ucbcba.joel.ucbcorreccionformato.FormalAspects;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
@@ -11,7 +10,7 @@ import java.util.List;
 
 @RestController
 public class BasicFormatController {
-    @RequestMapping(value = "/api/basicFormat/{fileName:.+}", method = RequestMethod.POST)
+    @PostMapping(value = "/api/basicFormat/{fileName:.+}")
     public List<BasicFormatReport> getBasicMisstakes(@PathVariable String fileName, @RequestParam(value="figureTableIndexPageEnd") String figureTableIndexPageEnd
             , @RequestParam(value="annexedPage") String annexedPage) {
         List<BasicFormatReport> basicFormatReports = new ArrayList<>();
