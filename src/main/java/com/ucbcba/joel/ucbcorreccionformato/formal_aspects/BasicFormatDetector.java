4bc156b71113428732b301cc47f0bb48d31836b4
neddyKG <gonzalezneddy@gmail.com>
2019-03-23 21:38:39 -0400

    fixed boolean

--- src/main/java/com/ucbcba/joel/ucbcorreccionformato/formal_aspects/BasicFormatDetector.java
+++ src/main/java/com/ucbcba/joel/ucbcorreccionformato/formal_aspects/BasicFormatDetector.java
@@ -83,7 +83,7 @@ public class BasicFormatDetector {
                 }
                 else {
                     List<WordsProperties> words = seeker.findWordsFromAPage( page,wordLine);
-                    if (words.size() != 0){
+                    if (words.isEmpty()){
                         if (words.get(words.size()-1).getY() > 720) {
                             isCorrectNumeration = true;
                         }
