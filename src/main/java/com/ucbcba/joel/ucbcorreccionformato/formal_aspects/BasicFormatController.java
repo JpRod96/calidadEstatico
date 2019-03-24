9eeb9f65652d98015688851c969376295441755f
neddyKG <gonzalezneddy@gmail.com>
2019-03-23 21:11:09 -0400

    changed exception to logger

--- src/main/java/com/ucbcba/joel/ucbcorreccionformato/formal_aspects/BasicFormatController.java
+++ src/main/java/com/ucbcba/joel/ucbcorreccionformato/formal_aspects/BasicFormatController.java
@@ -7,12 +7,15 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 public class BasicFormatController {
     @PostMapping(value = "/api/basicFormat/{fileName:.+}")
     public List<BasicFormatReport> getBasicMisstakes(@PathVariable String fileName, @RequestParam(value="figureTableIndexPageEnd") String figureTableIndexPageEnd
             , @RequestParam(value="annexedPage") String annexedPage) {
+        Logger LOGGER = Logger.getLogger("com.ucbcba.joel.ucbcorreccionformato.formal_aspects.BasicFormatController");
         List<BasicFormatReport> basicFormatReports = new ArrayList<>();
         String dirPdfFile = "uploads/"+fileName;
         PDDocument pdfdocument = null;
@@ -22,7 +25,7 @@ public class BasicFormatController {
             formatErrorDetector.analyzeBasicFormat(figureTableIndexPageEnd,annexedPage);
             basicFormatReports = formatErrorDetector.getBasicFormatReports();
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "context", e);
         }
         return basicFormatReports;
     }
