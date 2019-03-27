88b238424b7e101b69202e0e3fc2e80deb9adaf7
JpRod <jprodriguez60@gmail.com>
2019-03-27 15:08:49 -0400

    Clase GeneralIndexPageFormat ahora extendiendo de la nueva clase abstracta y lineas de codigo duplicadas con la clase BiographyPageFormat ahora estan contenidas en la nueva clase

--- src/main/java/com/ucbcba/joel/ucbcorreccionformato/FormatErrors/FormatRules/GeneralIndexPageFormat.java
+++ src/main/java/com/ucbcba/joel/ucbcorreccionformato/FormatErrors/FormatRules/GeneralIndexPageFormat.java
@@ -2,11 +2,9 @@ package com.ucbcba.joel.ucbcorreccionformato.FormatErrors.FormatRules;
 
 import com.ucbcba.joel.ucbcorreccionformato.FormatErrors.FormatControl.GeneralIndexFormat;
 import com.ucbcba.joel.ucbcorreccionformato.FormatErrors.HighlightsReport.FormatErrorReport;
-import com.ucbcba.joel.ucbcorreccionformato.General.GeneralSeeker;
 import com.ucbcba.joel.ucbcorreccionformato.General.ReportFormatError;
 import com.ucbcba.joel.ucbcorreccionformato.General.WordsProperties;
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
 
 import java.io.IOException;
 import java.text.Normalizer;
@@ -14,31 +12,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
-public class GeneralIndexPageFormat implements FormatRule {
-
-    private PDDocument pdfdocument;
-    private GeneralSeeker seeker;
-    private AtomicLong counter;
+public class GeneralIndexPageFormat extends EssentialDocFormat{
 
     public GeneralIndexPageFormat(PDDocument pdfdocument, AtomicLong counter) {
-        this.pdfdocument = pdfdocument;
-        this.seeker = new GeneralSeeker(pdfdocument);
-        this.counter = counter;
+        super(pdfdocument, counter);
     }
 
     @Override
     public List<FormatErrorReport> getFormatErrors(int page) throws IOException {
-        float pageWidth = pdfdocument.getPage(page-1).getMediaBox().getWidth();
-        float pageHeight = pdfdocument.getPage(page-1).getMediaBox().getHeight();
-        List<FormatErrorReport> formatErrors = new ArrayList<>();
-
-        PDFTextStripper pdfStripper = new PDFTextStripper();
-        pdfStripper.setStartPage(page);
-        pdfStripper.setEndPage(page);
-        pdfStripper.setParagraphStart("\n");
-        pdfStripper.setSortByPosition(true);
+        defaultGetFormatError(page);
+        
         //Recorre la página linea por linea
-        for (String line : pdfStripper.getText(pdfdocument).split(pdfStripper.getParagraphStart())) {
+        for (String line : pdfStripper.getText(pdfDocument).split(pdfStripper.getParagraphStart())) {
             String arr[] = line.split(" ", 2);
             // Condicional si encuentra una linea en blanco
             if (!arr[0].equals("")) {
