e588c4959a7ae73c666786afcc506940d3ef9ba9
JpRod <jprodriguez60@gmail.com>
2019-03-27 15:07:54 -0400

    Creada nueva clase abstracta que contenga los atributos y funcionalidad de escaneo del formato inicial de un documento

---
+++ src/main/java/com/ucbcba/joel/ucbcorreccionformato/FormatErrors/FormatRules/EssentialDocFormat.java
@@ -0,0 +1,44 @@
+package com.ucbcba.joel.ucbcorreccionformato.FormatErrors.FormatRules;
+
+import com.ucbcba.joel.ucbcorreccionformato.FormatErrors.HighlightsReport.FormatErrorReport;
+import com.ucbcba.joel.ucbcorreccionformato.General.GeneralSeeker;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by Jp on 27/03/2019.
+ */
+public abstract class EssentialDocFormat implements FormatRule{
+
+    PDDocument pdfDocument;
+    GeneralSeeker seeker;
+    AtomicLong counter;
+    float pageWidth;
+    float pageHeight;
+    List<FormatErrorReport> formatErrors;
+    PDFTextStripper pdfStripper;
+
+    public EssentialDocFormat(PDDocument pdfdocument, AtomicLong counter){
+        this.pdfDocument = pdfdocument;
+        this.seeker = new GeneralSeeker(pdfdocument);
+        this.counter = counter;
+    }
+
+    public void defaultGetFormatError(int page) throws IOException {
+        pageWidth = pdfDocument.getPage(page-1).getMediaBox().getWidth();
+        pageHeight = pdfDocument.getPage(page-1).getMediaBox().getHeight();
+        formatErrors = new ArrayList<>();
+
+        pdfStripper = new PDFTextStripper();
+        pdfStripper.setStartPage(page);
+        pdfStripper.setEndPage(page);
+        pdfStripper.setParagraphStart("\n");
+        pdfStripper.setSortByPosition(true);
+    }
+
+}
