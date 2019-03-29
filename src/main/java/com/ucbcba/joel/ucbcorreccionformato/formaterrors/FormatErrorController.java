6559931ee8dccaa2494f5c0ada3c59c671bf3ccc
JpRod <jprodriguez60@gmail.com>
2019-03-29 11:39:22 -0400

    Agregados librerias necesarias para los registros de errores

--- src/main/java/com/ucbcba/joel/ucbcorreccionformato/formaterrors/FormatErrorController.java
+++ src/main/java/com/ucbcba/joel/ucbcorreccionformato/formaterrors/FormatErrorController.java
@@ -8,6 +8,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 public class FormatErrorController {
